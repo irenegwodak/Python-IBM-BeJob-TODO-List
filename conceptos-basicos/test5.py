bd8970a1f84391e22a8e5b71048f9c5af65bb56b
Irene García <irenegwodak@gmail.com>
2024-05-20 17:02:22 +0200

    Solver test 5

--- conceptos-basicos/test5.py
+++ conceptos-basicos/test5.py
@@ -1,1 +1,10 @@
 # 5. Escriba un código que desordene al azar una lista.
+
+from random import shuffle
+
+def shuff(el):
+    shuffle(el)
+    return el 
+
+list = [1, 2, 3, 4, 5]
+print(shuff(list))
