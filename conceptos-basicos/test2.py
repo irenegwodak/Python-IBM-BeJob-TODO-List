8a2e152c2a8f6e7dad8d18f3f378948b4d7414e9
Irene García <irenegwodak@gmail.com>
2024-05-20 16:34:03 +0200

    Solve test2

--- conceptos-basicos/test2.py
+++ conceptos-basicos/test2.py
@@ -6,3 +6,13 @@ str = "código de práctica de prueba de geeks"
 # Salida:
 str = "geeks de prueba de práctica de código"
 
+def reverse(sentence):
+    words = sentence.split()
+    words.reverse()
+    reverse_sentence = " ".join(words)
+    return reverse_sentence
+
+
+sentence = "Ola k ase"
+reverse_sentence = reverse(sentence)
+print(reverse_sentence)
