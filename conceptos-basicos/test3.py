fb6a5e49fc05f7fbc047814a9d0b90cd08a5d921
Irene García <irenegwodak@gmail.com>
2024-05-20 16:42:46 +0200

    Solve test 3

--- conceptos-basicos/test3.py
+++ conceptos-basicos/test3.py
@@ -1,1 +1,18 @@
 # 3. Realizar una suma de los elementos de una tupla
+
+def add(tupla):
+    acc = 0
+    for number in tupla:
+        acc += number
+    return acc
+
+
+tupla = (2, 2, 2, 2, 2)
+result = add(tupla)
+
+print(result)
+
+#otra opción
+
+res = sum(tupla)
+print(res)
