37b3052d165dca55b6143f6b3832c811f1357c6e
Irene García <irenegwodak@gmail.com>
2024-05-21 10:31:12 +0200

    solve test 11

--- conceptos-basicos/test11.py
+++ conceptos-basicos/test11.py
@@ -1,1 +1,14 @@
 # 11. Escribir un algoritmo de ordenación para un conjunto de datos numéricos en Python.
+
+
+def ordenar(li):
+    new_li = li[:]
+    new_li.sort()
+    return new_li
+
+
+lista = [3, 7, 1, 9, 5]
+lista_ordenada = ordenar(lista)
+
+print("lista origianl", lista)
+print("lista ordenada", lista_ordenada)
