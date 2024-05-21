8db8d2c043b0eed56f38b4adf75a8b25e431378c
Irene García <irenegwodak@gmail.com>
2024-05-21 10:36:33 +0200

    Solve test 12

--- conceptos-basicos/test12.py
+++ conceptos-basicos/test12.py
@@ -1,1 +1,20 @@
 # 12. ¿Cuál de las siguientes declaraciones es inválida?
+
+# a) abc = 1.000.000
+
+# b) a b b c = 1000 2000 3000
+
+# c) a,b,c = 1,000,000
+
+# d) a_b_c = 1,000,000
+
+
+abc = 1.000.000 # <- no se permiten puntos para separar millares
+
+a b b c = 1000 2000 3000 # <- no se permiten espacios para declarar variables
+
+a,b,c = 1,000,000
+
+a_b_c = 1,000,000
+
+print(abc, a, b, c, a_b_c)
