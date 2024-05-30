9f6c59e76bfb9a0894dc4fd2dfd37ef6233b87cd
Irene García <irenegwodak@gmail.com>
2024-05-30 13:22:38 +0200

    Add new version: better workflow + add more user options to improve UX + refactor to cleancode

---
+++ ToDoList/improve-exercise/constants.py
@@ -0,0 +1,7 @@
+USER_MENU_OPTIONS = ("Agregar una tarea.", "Completar una tarea.",
+                "Eliminar una tarea.", "Salir.")
+
+CONTINUE_OPTION = {"si", "s"}
+NO_CONTINUE_OPTION = {"no", "n"}
+OTHER_OPTION = {"otro", "otra", "o"}
+USER_CHECK_OPTIONS = CONTINUE_OPTION | NO_CONTINUE_OPTION | OTHER_OPTION
